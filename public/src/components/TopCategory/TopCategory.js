65224770949d96ecb33c6ef7daa62bc3cbecf4eb
Liza1040 <maxsimova2002eliza@yandex.ru>
2022-10-03 01:32:41 +0300

    Добавлен компонент TopCategory

---
+++ public/src/components/TopCategory/TopCategory.js
@@ -0,0 +1,25 @@
+'use strict'
+
+import '../templates.js'
+
+export default class TopCategory {
+    #data
+    #parent
+
+    constructor(parent, value) {
+        this.#parent = parent;
+        this.#data = value;
+    }
+
+    render(context) {
+        const data = this.prepareCategory(context);
+        this.#parent.insertAdjacentHTML('afterbegin', window.Handlebars.templates['TopCategory.hbs'](data));
+    }
+
+    prepareCategory(context) {
+        console.log(context)
+        const data = {category: {}};
+        data.category = {...context};
+        return data;
+    }
+}
