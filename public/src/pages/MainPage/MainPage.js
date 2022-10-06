0f80fb3fa918b86e7c5d9aab9147ee415243dbbf
EuphoriaAbsorber <Scorpion1remeres@gmail.com>
2022-10-06 12:21:46 +0300

    added products from back

--- public/src/pages/MainPage/MainPage.js
+++ public/src/pages/MainPage/MainPage.js
@@ -6,7 +6,7 @@ import HeaderComponent from '../../components/Header/Header.js';
 import TopCategory from '../../components/TopCategory/TopCategory.js';
 import FooterComponent from '../../components/Footer/Footer.js';
 import ItemCard from '../../components/ItemCard/ItemCard.js';
-// import Req from "../../modules/ajax";
+import Req from "../../modules/ajax.js";
 
 export default class MainPage extends BasePage {
 
@@ -19,33 +19,58 @@ export default class MainPage extends BasePage {
 
     async render(context) {
 
-        // const r = new Req();
-        // const [status, photoPath] = await r.makeGetRequest('api/v1/');
-        // console.log(status);
-        //
-        // if (status === 200) {
-        //     console.log("session");
-        //
-        //     return;
-        // }
-        // console.log("no photo");
-
-
-        let key;
         super.render(context)
         this.headerComponent = new HeaderComponent(document.getElementById('header'));
         this.headerComponent.render(context.authorised);
         this.topComponent = new TopCategory(document.getElementById('catalog'));
         this.topComponent.render(context.topcategory);
-        for (key in context.itemCardsSales) {
-            this.itemCard = new ItemCard(document.getElementById(key));
-            this.itemCard.render(context.itemCardsSales[key]);
-        }
-        for (key in context.itemCardsPopular) {
-            this.itemCard = new ItemCard(document.getElementById(key));
-            this.itemCard.render(context.itemCardsPopular[key]);
-        }
         this.footerComponent = new FooterComponent(document.getElementById('footer'));
         this.footerComponent.render();
+
+        const r = new Req();
+        const [status, outD] = await r.makeGetRequest('api/v1/').catch((err) => console.log(err));
+
+        if(status === 200){
+            let key;
+            let card;
+            let num = 1;
+            let itemCards = outD.body
+            for (key in itemCards){
+                card = itemCards.body[key]
+                let discount = 100 - Math.round(card.DiscountPrice/card.Price*100);
+                let newCard = {     
+                            imgsrc: card.Imgsrc,
+                            discount: discount,
+                            price: card.DiscountPrice,
+                            salePrice: card.Price,
+                            cardTitle: card.Name,
+                            rating: card.Rating,
+                        };
+                if (discount == 0){newCard.salePrice = null}
+                let cardID = "salesCard" + String(num)
+                console.log(newCard.imgsrc)
+                this.itemCard = new ItemCard(document.getElementById(cardID));
+                this.itemCard.render(newCard);
+                num ++;
+            }
+            num = 1;
+
+            for (key in itemCards){
+                card = itemCards.body[key]
+                let discount = 100 - Math.round(card.DiscountPrice/card.Price*100);
+                let newCard = {     
+                            imgsrc: card.Imgsrc,
+                            discount: discount,
+                            price: card.DiscountPrice,
+                            salePrice: card.Price,
+                            cardTitle: card.Name,
+                            rating: card.Rating,
+                        };
+                let cardID = "popularCard" + String(num)
+                this.itemCard = new ItemCard(document.getElementById(cardID));
+                this.itemCard.render(newCard);
+                num ++;
+            }
+        } 
     }
 }
