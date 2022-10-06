78e37559305ae859a8b17f61a1d4bb66363165c3
Aint <pinevichv@gmail.com>
2022-10-07 02:36:41 +0300

    вернул ререш хедера на получение сессии

--- public/src/modules/refreshElements.js
+++ public/src/modules/refreshElements.js
@@ -5,6 +5,11 @@ import HeaderComponent from '../components/Header/Header.js';
 export default class RefreshEl {
 
     refreshHeader = (context) => {
+        const header = document.getElementById('header');
+        header.innerHTML = '';
+        context.headerComponent = new HeaderComponent(header);
+        context.headerComponent.render(context.authorised);
+
         if (context.authorised === true) {
             const headerProfile = document.querySelector('.header__profile');
             headerProfile.addEventListener("mouseover", async (event) => {
