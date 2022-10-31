57114155453d1f6432d5c86a74f7ecebb4adbae2
Liza1040 <95960035+Liza1040@users.noreply.github.com>
2022-10-31 23:50:20 +0300

    Base 2.7 service worker (#19)
    * changed url for getproducts
    
    * changed path for get products
    
    * Добавлена проверка на пустое поле и отрисовка ошибок после нажатия кнопки
    
    * Поправлена валидация на странице входа, добавлен обработчик события focusin
    
    * Создан класс для отрисовки ошибок и вынесен в отдельный модуль
    
    * Поправлена валидация на странице регистрации, добавлен обработчик события focusin
    
    * Функция DeleteErrorMessage вынесена в модуль ErrorMessage
    
    * Добавлен async на обработчики события  focusin
    
    * Delete templates.js
    
    * Delete templates.js
    
    * Поправлены ошибки, отловленные  linter
    
    * Проверка линтера
    
    * Проверка linter
    
    * Проверка linter
    
    * Сообщения об ошибке вынесено в константу.  Линтер перенесен в devDependencies.
    
    * переписана validate
    
    так же validation и errorMessage импортируются как синглтоны, пофикшены ошибки и линтеры
    
    * убрал auth: true в конфиге
    
    * исрпавлено извдечение данных из data при отправке запроса
    
    * исрпавил парсинг data v2
    
    * испавление удаления EventListener
    
    * Поправлен JSDoc.
    
    * Добавлен service worker для работы без интернета
    
    * Добавлен обработчик на событие load
    
    * Добавлен обработчик события activate
    
    * Добавление обновления кеша после похода в сеть
    
    * Тестирование SW
    
    * Исправлены ошибки, найденные линтером
    
    * Поправлены ошибки, найденные линтером
    
    * Поправлены синтаксические ошибки
    
    * Поправлена синтаксическая ошибка
    
    * Поправлена ошибка, найденная линтером
    
    * Добавлена отправка url в разных состояниях  SW. Теперь SW сначала идет в сеть, а потом в кеш.
    
    * Убран отладочный код
    
    * Поправлены ошибки, найденные линтером
    
    * Поправлены ошибки, найденные линтером
    
    * Поправлены ошибки, найденные линтером
    
    * Убран async у функции регистрации sw и обработчик события load для ее вызова. Убран console.log при installing.
    
    Co-authored-by: EuphoriaAbsorber <Scorpion1remeres@gmail.com>
    Co-authored-by: Aint <pinevichv@gmail.com>

--- public/src/index.js
+++ public/src/index.js
@@ -141,3 +141,33 @@ const checkSession = async () => {
 
 window.addEventListener('DOMContentLoaded', checkSession, {once: true});
 config.currentPage = config.header.main.render(config);
+
+// Регистрация Service Worker
+const registerServiceWorker = () => {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/sw.js', {scope: '/'})
+            .then((registration) => {
+                const data = {
+                    type: 'CACHE_URLS',
+                    payload: [
+                        location.href,
+                        ...performance.getEntriesByType('resource').map((r) => r.name),
+                    ],
+                };
+                if (registration.installing) {
+                    registration.installing.postMessage(data);
+                } else if (registration.waiting) {
+                    registration.waiting.postMessage(data);
+                    console.log('Service worker installed');
+                } else if (registration.active) {
+                    registration.active.postMessage(data);
+                    console.log('Service worker active');
+                }
+            })
+            .catch((error) => console.log(`SW registration failed with ${error}`));
+    } else {
+        console.log('Service Workers are not supported in this browser');
+    }
+};
+
+registerServiceWorker();
