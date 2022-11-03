aaa2175d30039cab7f6cf736550999e979d7175e
Aint <pinevichv@gmail.com>
2022-11-03 16:54:24 +0300

    настрока редима сборки
    так же добавлен ImageMinimizerPlugin, babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
     entry: './public/src/index.js',
@@ -12,7 +12,7 @@ module.exports = {
             {test: /\.svg$/, use: 'svg-inline-loader'},
             {
                 test: /\.s[ac]ss$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
             },
             {test: /\.(js)$/, use: 'babel-loader'},
             {test: /\.(json)$/, use: 'cson-loader'},
@@ -20,9 +20,19 @@ module.exports = {
             {test: /\.hbs$/, loader: 'handlebars-loader'},
             {test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset/resource'},
             {test: /\.(woff|woff2|eot|ttf|otf)$/i, type: 'asset/resource'},
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        cacheDirectory: true,
+                    },
+                },
+            },
         ],
     },
-    devtool: 'inline-source-map',
+    devtool: 'source-map',
     devServer: {
         static: './dist',
         hot: true,
@@ -44,13 +54,45 @@ module.exports = {
                 },
             },
         },
+        minimizer: [
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.sharpMinify,
+                    options: {
+                        encodeOptions: {
+                            jpeg: {
+                                quality: 100,
+                            },
+                            webp: {
+                                lossless: true,
+                            },
+                            avif: {
+                                lossless: true,
+                            },
+                            png: {},
+                            gif: {},
+                        },
+                    },
+                },
+            }),
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.svgoMinify,
+                    options: {
+                        encodeOptions: {
+                            multipass: true,
+                            plugins: [
+                                'preset-default',
+                            ],
+                        },
+                    },
+                },
+            }),
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.hbs',
-        }),
-        new webpack.EnvironmentPlugin({
-            'NODE_ENV': 'development',
         }),
         new CopyWebpackPlugin({
             patterns: [
@@ -62,7 +104,7 @@ module.exports = {
         }),
         new FaviconsWebpackPlugin(path.resolve(__dirname, 'public/img/favicon.png')),
         new MiniCssExtractPlugin({
-            filename: '[name].css',
+            filename: '[name].[contenthash].css',
             chunkFilename: '[id].css',
             ignoreOrder: false,
         }),
