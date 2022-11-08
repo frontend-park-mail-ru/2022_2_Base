0b15612dd3d345a0838b88f3745aacede64ac13a
Aint <pinevichv@gmail.com>
2022-11-08 21:15:48 +0300

    испрваление рефреша хедера

--- public/src/modules/refreshElements.js
+++ public/src/modules/refreshElements.js
@@ -78,6 +78,7 @@ class RefreshEl {
         router.register(config.header.signup.href, RegisterPage);
         router.remove(config.header.user.href);
         router.refresh(config);
+        this.refreshHeader(userStore.getContext(userStore._storeNames.isAuth));
     }
 }
 
