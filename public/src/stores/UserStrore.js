b90f57b26953e7e768f688e994d5895712b01525
Aint <pinevichv@gmail.com>
2022-11-18 04:08:41 +0300

    соединяю пользователя 13

--- public/src/stores/UserStrore.js
+++ public/src/stores/UserStrore.js
@@ -513,7 +513,6 @@ class UserStore extends BaseStore {
         userData.paymentmethods.push(data);
         // console.log('data', data);
         console.log(userData);
-        paymentCard/
         //console.log(userData.paymentmethods);
         const [status] = await request.makePostRequest(config.api.profile, userData)
             .catch((err) => console.log(err));
