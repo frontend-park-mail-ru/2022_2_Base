668db32c5080bbc19e9955490c86fa9f4bbf967f
Aint <pinevichv@gmail.com>
2022-11-23 23:39:42 +0300

    фикс пустой карты

--- public/src/pages/CartPage/CartPage.js
+++ public/src/pages/CartPage/CartPage.js
@@ -112,7 +112,7 @@ export default class CartOrderPage extends BasePage {
                 });
             }
             context.isAuth = userStore.getContext(userStore._storeNames.isAuth);
-            // context.isAuth = true; // FIX
+            context.isAuth = true; // FIX
             if (context.isAuth) {
                 context.avatar = userStore.getContext(userStore._storeNames.avatar);
                 context.username = userStore.getContext(userStore._storeNames.name);
@@ -442,8 +442,15 @@ export default class CartOrderPage extends BasePage {
                     (Number(time[1].split(':')[0]) + Number(time[0].split(':')[0])) / 2 % 24,
                     0)).toJSON();
 
-                orderData.card = parseInt(document.querySelector('.payment-method_cart')
-                    .id.split('/')[1]) ?? config.states.noPayCardId;
+                console.log('element card', document.querySelector('.payment-method_cart')
+                    .id.split('/')[1]);
+                console.log('element card id', parseInt(document.querySelector('.payment-method_cart')
+                    .id.split('/')[1]));
+
+
+                orderData.card = document.querySelector('.payment-method_cart')
+                    .id.split('/')[1] ?? config.states.noPayCardId;
+                orderData.card = parseInt(orderData.card);
 
                 cartAction.makeOrder(orderData);
             } else {
