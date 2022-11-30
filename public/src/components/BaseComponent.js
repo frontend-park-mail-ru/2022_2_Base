5efe7cb1f62bd838ebd5fa51e08cf8aed8fe0749
Aint <pinevichv@gmail.com>
2022-11-30 12:04:47 +0300

    поправил jsDoc

--- public/src/components/BaseComponent.js
+++ public/src/components/BaseComponent.js
@@ -30,7 +30,7 @@ export default class BaseComponent {
      * Метод, отрисовывающий компонент.
      * @param {any} context контекст данных для компонента
      * @param {HandlebarsTemplateDelegate} templateName скомпилированный шаблон шаблона
-     * @param {InsertPosition} insertInPlace куда вставлять относительно _parent элемента
+     * @param {InsertPosition} insertInPlace место вставки относительно _parent элемента
      */
     render(context, templateName, insertInPlace = 'afterbegin') {
         this._parent.insertAdjacentHTML(insertInPlace,
