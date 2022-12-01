d04d8a49147e3670b2df4b7ddf1e98b61ae5df8e
Aint <pinevichv@gmail.com>
2022-12-01 18:11:55 +0300

    fix avatar

--- public/src/stores/UserStore.js
+++ public/src/stores/UserStore.js
@@ -330,13 +330,8 @@ class UserStore extends BaseStore {
 
         this._storage.set(this._storeNames.responseCode, status);
         if (status === config.responseCodes.code200) {
-            if (avatar) {
-                this._storage.set(this._storeNames.avatar,
-                    URL.createObjectURL(avatar));
-            } else {
-                this._storage.set(this._storeNames.avatar,
-                    'img/UserPhoto.webp');
-            }
+            this._storage.set(this._storeNames.avatar,
+                URL.createObjectURL(avatar));
         }
     }
 
