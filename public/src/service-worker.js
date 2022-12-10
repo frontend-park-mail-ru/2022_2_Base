0139dfef7183c7a33705dbbcc6558fe6b5f33ef0
Liza1040 <maxsimova2002eliza@yandex.ru>
2022-12-10 13:56:51 +0300

    Fix sw

--- public/src/service-worker.js
+++ public/src/service-worker.js
@@ -27,54 +27,54 @@ this.addEventListener('activate', (event) => {
  * @description Подписываемся на событиие отправки браузером запроса к серверу
 */
 self.addEventListener('fetch', (event) => {
-    event.respondWith(networkFirst(event.request)
+    event.respondWith(//networkFirst(event.request)
         // Ищем ресурс в кэше
-        // caches.match(event.request)
-        //     .then((cachedResponse) => {
-        //         if (navigator.onLine) {
-        //             return fetch(event.request) // Получить данные из сети
-        //                 .then((res) => {
-        //                     const resClone = res.clone();
-        //                     putInCache(event.request, resClone);
-        //                     return res;
-        //                 })
-        //                 .catch((err) => console.error(err));
-        //         }
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                if (navigator.onLine) {
+                    return fetch(event.request) // Получить данные из сети
+                        .then((res) => {
+                            const resClone = res.clone();
+                            putInCache(event.request, resClone);
+                            return res;
+                        })
+                        .catch((err) => console.error(err));
+                }
 
-        //         if (cachedResponse) {
-        //             return cachedResponse; // Получить из кеша
-        //         }
-        //         console.log("not found")
-        //         return caches.match('/error404');
-        //     })
-        //     .catch((err) => {
-        //         console.log(err.stack || err);
-        //     }),
+                if (cachedResponse) {
+                    return cachedResponse; // Получить из кеша
+                }
+                console.log("not found")
+                return caches.match('/error404');
+            })
+            .catch((err) => {
+                console.log(err.stack || err);
+            }),
     );
 });
 
-async function networkFirst(request) {
-    const cache = caches.open(CACHE_NAME);
-    try {
-        if (navigator.onLine) {
-            return fetch(request) // Получить данные из сети
-                .then((res) => {
-                    const resClone = res.clone();
-                    putInCache(request, resClone);
-                    return res;
-                })
-                .catch((err) => console.error(err));
-        }
-    } catch {
-        let cached;
-        try {
-            cached = await cache.match(request);
-        } catch {
-            return new Response(null, { status: 404, statusText: 'Not Found' });
-        }
-        return cached;
-    }
-}
+// async function networkFirst(request) {
+//     const cache = caches.open(CACHE_NAME);
+//     try {
+//         if (navigator.onLine) {
+//             return fetch(request) // Получить данные из сети
+//                 .then((res) => {
+//                     const resClone = res.clone();
+//                     putInCache(request, resClone);
+//                     return res;
+//                 })
+//                 .catch((err) => console.error(err));
+//         }
+//     } catch {
+//         let cached;
+//         try {
+//             cached = await cache.match(request);
+//         } catch {
+//             return new Response(null, { status: 404, statusText: 'Not Found' });
+//         }
+//         return cached;
+//     }
+// }
 
 const putInCache = async (request, response) => {
     const cache = await caches.open(CACHE_NAME);
