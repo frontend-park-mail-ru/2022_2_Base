027fcfa766503150a8c5915ae94247ac3d1803d0
Aint <pinevichv@gmail.com>
2022-12-11 01:55:53 +0300

    skip + claim in sw

--- public/src/service-worker.js
+++ public/src/service-worker.js
@@ -7,11 +7,10 @@ const imageRegRex = /.webp|.svg|.jpg|.jpeg|.gif|.png/;
  * @description Подписываемся на событиие установки сервис-воркера
  */
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(urls)),
     );
-    console.log(1); //fix
+    return self.skipWaiting();
 });
 
 /**
@@ -19,6 +18,8 @@ self.addEventListener('install', (event) => {
  */
 self.addEventListener('activate', (event) => {
     event.waitUntil(enableNavigationPreload());
+
+    return self.clients.claim();
     // const expectedCache = Object.keys(CACHE_NAME).map((key) => CACHE_NAME[key]);
 
     // event.waitUntil(
