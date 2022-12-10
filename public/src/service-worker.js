86b796c332e18edc5e1d2c0a2db9e7cbe9f75fc0
Aint <pinevichv@gmail.com>
2022-12-10 19:29:44 +0300

    enableNavigationPreload + cacheFirst

--- public/src/service-worker.js
+++ public/src/service-worker.js
@@ -1,22 +1,23 @@
 const CACHE_NAME = 'base-v1';
 
 const urls = [];
+const imageRegRex = /.webp|.svg|.jpg|.jpeg|.gif|.png/;
 
 /**
  * @description Подписываемся на событиие установки сервис-воркера
-*/
+ */
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(urls)),
     );
-    console.log(1);
+    console.log(1); //fix
 });
 
 /**
  * @description Подписываемся на событиие активации сервис-воркера
-*/
-this.addEventListener('activate', (event) => {
-    console.log(4);
+ */
+self.addEventListener('activate', (event) => {
+    event.waitUntil(enableNavigationPreload());
     // const expectedCache = Object.keys(CACHE_NAME).map((key) => CACHE_NAME[key]);
 
     // event.waitUntil(
@@ -36,9 +37,9 @@ this.addEventListener('activate', (event) => {
 /**
  * @description Подписываемся на событиие получения сообщения со списком ресурсов,
  * которые надо кешировать при первом посещении страницы
-*/
+ */
 self.addEventListener('message', (event) => {
-    console.log(2);
+    console.log(2); //fix
     if (event.data.type === 'CACHE_URLS') {
         event.waitUntil(
             caches.open(CACHE_NAME)
@@ -48,41 +49,49 @@ self.addEventListener('message', (event) => {
                 .catch((error) => console.log(`Error adding to cache ${error}`)),
         );
     }
-    console.log(3);
+    console.log(3); //fix
 });
 
 /**
  * @description Подписываемся на событиие отправки браузером запроса к серверу
-*/
+ */
 self.addEventListener('fetch', (event) => {
-    event.respondWith(networkFirst(event.request)
-        // Ищем ресурс в кэше
-        // caches.match(event.request)
-        //     .then((cachedResponse) => {
-        //         if (navigator.onLine) {
-        //             return fetch(event.request) // Получить данные из сети
-        //                 .then((res) => {
-        //                     const resClone = res.clone();
-        //                     putInCache(event.request, resClone);
-        //                     return res;
-        //                 })
-        //                 .catch((err) => console.error(err));
-        //         }
-
-        //         if (cachedResponse) {
-        //             return cachedResponse; // Получить из кеша
-        //         }
-        //         console.log("not found")
-        //         return caches.match('/error404');
-        //     })
-        //     .catch((err) => {
-        //         console.log(err.stack || err);
-        //     }),
-    );
+    if (imageRegRex.test(event.request)) {
+        event.respondWith(cacheFirst(event));
+    } else {
+        event.respondWith(networkFirst(event));
+    }
     return true;
+    // Ищем ресурс в кэше
+    // caches.match(event.request)
+    //     .then((cachedResponse) => {
+    //         if (navigator.onLine) {
+    //             return fetch(event.request) // Получить данные из сети
+    //                 .then((res) => {
+    //                     const resClone = res.clone();
+    //                     putInCache(event.request, resClone);
+    //                     return res;
+    //                 })
+    //                 .catch((err) => console.error(err));
+    //         }
+
+    //         if (cachedResponse) {
+    //             return cachedResponse; // Получить из кеша
+    //         }
+    //         console.log("not found")
+    //         return caches.match('/error404');
+    //     })
+    //     .catch((err) => {
+    //         console.log(err.stack || err);
+    //     }),
 });
 
-async function networkFirst(request) {
+/**
+ * @description Первый запрос в сеть
+ * @param {Event} event
+ */
+async function networkFirst(event) {
+    const {request} = event;
     const cache = await caches.open(CACHE_NAME);
     try {
         const response = await fetch(request);
@@ -101,15 +110,71 @@ async function networkFirst(request) {
         //         .catch((err) => console.error(err));
         // }
     } catch {
-        let cachedResponse;
+        // let cachedResponse;
         try {
-            cachedResponse = await cache.match(request);
+            const cachedResponse = await cache.match(request);
+            if (cachedResponse) {
+                return cachedResponse;
+            }
         } catch {
-            return new Response(null, { status: 404, statusText: 'Not Found' });
+            const response = await event.preloadResponse;
+            if (response) {
+                return response;
+            } else {
+                return new Response(null, {status: 404, statusText: 'Not Found'});
+            }
         }
-        return cachedResponse;
+        // return cachedResponse;
     }
 }
+
+/**
+ * @description Первый запрос в сеть
+ * @param {Event} event
+ */
+async function cacheFirst(event) {
+    const {request} = event;
+    const cache = await caches.open(CACHE_NAME);
+    try {
+        const cachedResponse = await cache.match(request);
+        if (cachedResponse) {
+            return cachedResponse;
+        }
+
+        const response = await event.preloadResponse;
+        if (response) {
+            return response;
+        }
+        // if (navigator.onLine) {
+        //     return fetch(request) // Получить данные из сети
+        //         .then((res) => {
+        //             const resClone = res.clone();
+        //             await cache.put(request, resClone);
+        //             return res;
+        //         })
+        //         .catch((err) => console.error(err));
+        // }
+    } catch {
+        // let cachedResponse;
+        try {
+            const response = await fetch(request);
+
+            if (navigator.onLine) {
+                await cache.put(request, response.clone());
+            }
+            return response;
+        } catch {
+            return new Response(null, {status: 404, statusText: 'Not Found'});
+        }
+        // return cachedResponse;
+    }
+}
+
+const enableNavigationPreload = async () => {
+    if (self.registration.navigationPreload) {
+        await self.registration.navigationPreload.enable();
+    }
+};
 
 // const putInCache = async (request, response) => {
 //     const cache = await caches.open(CACHE_NAME);
