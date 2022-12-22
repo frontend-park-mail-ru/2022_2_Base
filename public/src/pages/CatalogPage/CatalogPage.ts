d330a41436c0bbbd0247b486e94ef16c1ad90b11
Aint <pinevichv@gmail.com>
2022-12-22 16:46:52 +0300

    фикс иконки сортировки

--- public/src/pages/CatalogPage/CatalogPage.ts
+++ public/src/pages/CatalogPage/CatalogPage.ts
@@ -246,8 +246,8 @@ export default class CatalogPage extends BasePage {
                 const isLowToHighPrice =
                     window.location.search.includes(config.queryParams.sort.priceDown);
                 isLowToHighPrice ?
-                    this.sortOrder.classList.add('rotate-img-180') :
-                    this.sortOrder.classList.remove('rotate-img-180');
+                    this.sortOrder.classList.remove('rotate-img-180') :
+                    this.sortOrder.classList.add('rotate-img-180');
                 this.getSortByPrice(isLowToHighPrice);
                 break;
             }
